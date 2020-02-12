059a5b8f163d4107149d6670a34827bf6b149c5e
lucyayala <lucy.ayala@my.tccd.edu>
2020-02-12 05:49:06 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+
+/* 
+Date: 02/10/2020 IDE: Repl.it tool 
+*/  
+#include <iostream>
+using namespace std;
+
+double weeklyPay(double, double,string,int);// function prototype
+
+int main( )
+{    
+         //a function call for each person - no input from the console for part 1
+
+         cout << weeklyPay (32.5, 100.25,"Tyson",1) << endl;
+
+
+         // re-type " or ' if necessary 
+
+  return 0;
+}
+
+
+double weeklyPay(double hours, double rate, string name, int empType)
+{// complete the function definition
+
+            double netPay = 0.0;
+
+
+            return netPay; 
+
+}
